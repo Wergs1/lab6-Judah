97ea066b07d44a40b2b092dbe1474244e8666ab0
Wergs1 <wergheaftman@gmail.com>
2023-10-25 10:05:23 -0400

    Please work

---
+++ decode.py
@@ -0,0 +1,18 @@
+#decode function
+run = True
+
+
+def encode(inpu):
+    ret = ""
+    for i in range(0, len(inpu)):
+        temp_num = int(inpu[i]) + 3
+        if temp_num > 9:
+            ret += str((10-temp_num))
+        else:
+            ret += str((temp_num))
+    return ret
+
+
+while run == True:
+    raw = input("Enter value to encode: ")
+    print(str(encode(raw)))
