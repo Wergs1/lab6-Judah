5bd6519062769f496d367621ae82f1683c1347ea
Wergs1 <wergheaftman@gmail.com>
2023-10-25 10:24:15 -0400

    Updated encoder

--- encode.py
+++ encode.py
@@ -5,9 +5,9 @@ run = True
 def encode(inpu):
     ret = ""
     for i in range(0, len(inpu)):
-        temp_num = int(inpu[i]) + 3
+        temp_num = int(inpu[i]) + 3 #basic encode (raw)
         if temp_num > 9:
-            ret += str((10-temp_num))
+            ret += str((temp_num-10)) #Makes encoded number (final)
         else:
             ret += str((temp_num))
     return ret
